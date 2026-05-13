3f03c9a163fd34ec74894255fb3fba33d46ab4cc
William L Johnson <william.lawrence.johnson@gmail.com>
2026-05-12 17:16:55 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        return list(process(strs))
+
+def process(data):
+    N = len(data)
+    i = 0
+
+    while i < N:
+        x, X = data[i], hash(data[i])
+        a, b = i, i + 1
+
+        for j in range(i + 1, N):
+            y, Y = data[j], hash(data[j])
+
+            if X == Y:
+                data[b], data[j] = data[j], data[b]
+                b += 1
+
+        yield list(data[a:b])
+        i = b
+
+def hash(value):
+    return "".join(sorted([x for x in value]))
