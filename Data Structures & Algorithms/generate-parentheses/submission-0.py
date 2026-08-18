9913c5a1ec87d632b95f27aa141a002b5c187940
William L Johnson <william.lawrence.johnson@gmail.com>
2026-08-18 16:27:45 -0700

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        return list(process(n, n, []))
+
+OPEN = "("
+CLOSE = ")"
+
+def process(open_count, close_count, path):
+
+    if not open_count and not close_count:
+        yield "".join(path)
+        return
+
+    if open_count:
+        path.append(OPEN)
+        yield from process(open_count - 1, close_count, path)
+        path.pop()
+
+    if close_count and close_count > open_count:
+        path.append(CLOSE)
+        yield from process(open_count, close_count - 1, path)
+        path.pop()
