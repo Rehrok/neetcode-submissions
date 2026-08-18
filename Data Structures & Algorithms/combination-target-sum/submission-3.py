56bcdff6b269b6bef03b034bea3baca156dd7fa2
William L Johnson <william.lawrence.johnson@gmail.com>
2026-08-17 17:11:08 -0700

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        return list(process(sorted(nums), target, 0, 0, defaultdict(int)))
+
+def process(data, target, index, partial, counts):
+
+    if partial > target:
+        return
+
+    if partial == target:
+        yield build_combination(data, counts)
+        return
+
+    if index >= len(data):
+        return
+
+    yield from process(data, target, index + 1, partial, counts)
+
+    x = data[index]
+
+    while partial < target:
+        partial += x
+        counts[index] += 1
+        yield from process(data, target, index + 1, partial, counts)
+
+    counts[index] = 0
+
+def build_combination(data, counts):
+    working = []
+    for key, value in counts.items():
+        for _ in range(value):
+            working.append(data[key])
+
+    return working
